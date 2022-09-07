8e47fb4bd758fc93ee03c2569aa17252675ed496
Zahid-BM <bmzahidulislam>
2022-09-07 21:53:50 +0600

    Alhamdulillah all cards are responsive in the latestItems part and hover effect added

--- src/Pages/Home/LatestItems/LatestItems.js
+++ src/Pages/Home/LatestItems/LatestItems.js
@@ -5,8 +5,16 @@ import SingleLatestItem from './SingleLatestItem';
 const LatestItems = () => {
     const [allFurniture] = UseAllFurniture();
     return (
-        <>
-            <div className="grid grid-cols-3 gap-4">
+        <div className='container mx-auto my-16'>
+            <div class="text-center">
+                <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">From the blog</h1>
+
+                <p class="max-w-lg mx-auto mt-4 text-gray-500">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt, laudantium
+                    quia tempore delect
+                </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-16 ">
                 {
                     allFurniture.map(singleFurniture => <SingleLatestItem
                         key={singleFurniture._id}
@@ -16,7 +24,7 @@ const LatestItems = () => {
                 }
             </div>
 
-        </>
+        </div>
     );
 };
 
--- src/Pages/Home/LatestItems/SingleLatestItem.js
+++ src/Pages/Home/LatestItems/SingleLatestItem.js
@@ -3,19 +3,21 @@ import React from 'react';
 const SingleLatestItem = ({ latestFurniture }) => {
     const { id, picture, name, model, price, warranty, description } = latestFurniture;
     return (
-        <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-            <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{
-                backgroundImage: `url(${picture})`,
-                backgroundSize: 'contain',
-                backgroundRepeat: 'no-repeat'
-            }}></div>
+        <div>
+            <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto hover:transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 duration-300 my-4">
+                <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{
+                    backgroundImage: `url(${picture})`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                }}></div>
 
-            <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-                <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">{name}</h3>
+                <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+                    <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">{name}</h3>
 
-                <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
-                    <span class="font-bold text-gray-800 dark:text-gray-200">{price}</span>
-                    <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                    <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+                        <span class="font-bold text-gray-800 dark:text-gray-200">{price}</span>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                    </div>
                 </div>
             </div>
         </div>
